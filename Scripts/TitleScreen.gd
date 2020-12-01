d2da142d4ec548432668f8e2e0820adc902dd5f4
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2020-12-01 10:38:45 +0100

    Add prototype help screen

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -2,10 +2,10 @@ extends Control
 
 onready var tree = get_tree()
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+# Menu screens
+onready var main = $"Main Menu"
+onready var options = $Options
+onready var help = $Help
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +13,20 @@ func _ready():
 
 func quit():
 	tree.quit()
+
+func hideScreens():
+	main.visible = false
+	options.visible = false
+	help.visible = false
+
+func showHelp():
+	hideScreens()
+	help.visible = true
+
+func showOptions():
+	hideScreens()
+	options.visible = true
+
+func backToMain():
+	hideScreens()
+	main.visible = true
