e6f23e22f4a471558f950e7365342d29b10e21c0
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2020-12-03 11:41:12 +0100

    Add bed object
    Place bed on random floor and move player next to bed

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -3,12 +3,15 @@ extends Spatial
 
 const FLOOR_HEIGHT = 3
 
+onready var player = $Player
+
 onready var floorLbl = $"UI/Element Counters/Floors"
 onready var lightsLbl = $"UI/Element Counters/Lights"
 
 var floorObj = preload("res://World Elements/Floor.tscn")
 var stairsObj = preload("res://World Elements/Stairs.tscn")
 var lightObj = preload("res://World Elements/Light.tscn")
+var bedObj = preload("res://World Elements/Bed.tscn")
 
 var bedFloor = 0
 var floorCount = 0
@@ -22,12 +25,22 @@ func _ready():
 	rng.randomize()
 
 	floorCount = rng.randi_range(1, 6)
-	bedFloor = rng.randi_range(1, floorCount)
+	bedFloor = rng.randi_range(0, floorCount - 1)
 	lightCount = 0
 
 	deactivatedLights = 0
 
 	var size = Vector3(Settings.maxWidth, 1, Settings.maxLength)
+
+	var bedpos = Vector3(
+		rng.randi_range(-size.x / 2, size.x / 2),
+		bedFloor * FLOOR_HEIGHT,
+		rng.randi_range(-size.z / 2, size.z / 2)
+	)
+	var bed = bedObj.instance()
+	bed.translate(bedpos)
+	add_child(bed)
+	player.translate(bedpos + Vector3(0, 0, 3))
 
 	for i in range(floorCount):
 		# Floor
