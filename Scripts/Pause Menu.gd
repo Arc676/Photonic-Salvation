5ac1454b15805a081997f135c12c73a81083a805
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2020-12-04 10:36:44 +0100

    Add pause menu
    Can return to main

---
+++ Scripts/Pause Menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+onready var tree = get_tree()
+onready var pauseMenu = $"."
+
+func _process(_delta):
+	if Input.is_action_just_released("ui_cancel"):
+		tree.paused = !tree.paused
+		pauseMenu.visible = tree.paused
+		Input.set_mouse_mode(
+			Input.MOUSE_MODE_VISIBLE if tree.paused else
+			Input.MOUSE_MODE_CAPTURED
+		)
+
+func backToMain():
+	tree.paused = false
+	tree.change_scene("res://Scenes/Main Menu.tscn")
